10d1fbc653607ff4187f19b4c5f87455c18b5624
root <acridbrimistic@protonmail.com>
2019-06-10 19:05:21 +0300

    removed clutter

---
+++ scr.js
@@ -0,0 +1,17 @@
+
+function b1c() {
+
+window.open("https://acridbrimistic.github.io/conquest.html");
+}
+function b2c() {
+
+window.open("https://acridbrimistic.github.io/campaign.html");
+}
+function b3c() {
+
+window.open("https://acridbrimistic.github.io/general.html");
+}
+function b4c() {
+
+window.open("https://acridbrimistic.github.io/other.html");
+}
